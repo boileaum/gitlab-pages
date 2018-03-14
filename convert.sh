8e350915fc63888fba41fe7979dbf12ea97f42db
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2018-03-14 11:00:58 +0100

    Update notebook

--- convert.sh
+++ convert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jupyter nbconvert --to slides gitlab-pages.ipynb --reveal-prefix "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0" --execute
-jupyter nbconvert --to html gitlab-pages.ipynb --execute
+jupyter nbconvert --to slides gitlab-pages.ipynb --reveal-prefix "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0" --execute --allow-errors
+jupyter nbconvert --to html gitlab-pages.ipynb --execute --allow-errors
