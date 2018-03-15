c883f3f9c9d11c7e3adabe5513ab0b9b1a7ae0b4
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2018-03-15 19:02:16 +0100

    Update notebook with new images

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-jupyter nbconvert --to slides gitlab-pages.ipynb --reveal-prefix "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0" --execute --allow-errors
+jupyter nbconvert --to slides gitlab-pages.ipynb --execute --allow-errors --reveal-prefix "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0"
 jupyter nbconvert --to html gitlab-pages.ipynb --execute --allow-errors
+mv gitlab-pages.slides.html index.html
+rm -rf public
+mkdir public
+mv index.html gitlab-pages.html public/
+cp -r images public/
+mv public/images/favicon.ico public/
