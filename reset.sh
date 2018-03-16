ad9d42fc98c2bfc339b8158e577b4f48a5b0b59a
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2018-03-16 10:56:31 +0100

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -3,9 +3,9 @@
 # Reset git repo to start a new demo
 
 git checkout master
-git branch -d dev
+git branch -D dev
 git rm .gitlab-ci.yml
 git commit -m "Remove .gitlab-ci.yml to restart a new demo"
-git remote remove
+git remote remove demo
 git push origin
 
