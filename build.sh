9258d6625753f45c04eda72aede853942085bef6
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2018-03-16 08:40:03 +0100

    Remove --execute from build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-jupyter nbconvert --to slides gitlab-pages.ipynb --execute --allow-errors --reveal-prefix "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0"
-jupyter nbconvert --to html gitlab-pages.ipynb --execute --allow-errors
+jupyter nbconvert --to slides gitlab-pages.ipynb --reveal-prefix "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0"
+jupyter nbconvert --to html gitlab-pages.ipynb
 mv gitlab-pages.slides.html index.html
 rm -rf public
 mkdir public
