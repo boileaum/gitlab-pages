b9cae2a3e3df6db3e9e463d699ddf07d2395e286
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2018-03-16 08:45:26 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 jupyter nbconvert --to slides gitlab-pages.ipynb --reveal-prefix "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0"
 jupyter nbconvert --to html gitlab-pages.ipynb
 mv gitlab-pages.slides.html index.html
